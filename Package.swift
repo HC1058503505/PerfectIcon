968b960e6f7bbc876ef8a415519a2f107358c593
HC1058503505 <1058503505@qq.com>
2017-04-14 13:30:17 +0800

    修改package

--- Package.swift
+++ Package.swift
@@ -5,9 +5,6 @@ let package = Package(
     name: "PerfectIcon",
     targets: [],
     dependencies: [
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 2),
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-MongoDB.git", majorVersion: 2),
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-MySQL.git", majorVersion: 2),
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-SQLite.git", majorVersion: 2),
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 2)
     ]
 )
